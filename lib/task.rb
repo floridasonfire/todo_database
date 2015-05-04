28d01b7e5fc408cde0498f9327768ea00b103d13
Kevin Chen & jessica engel <student@epicodus.com>
2015-05-04 14:28:56 -0700

    order tasks by due date

--- lib/task.rb
+++ lib/task.rb
@@ -1,10 +1,10 @@
 class Task
-  attr_reader(:description, :list_id)
-
+  attr_reader(:description, :list_id, :due_date)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @due_date = attributes.fetch(:due_date)
   end
 
   define_method(:description) do
@@ -12,18 +12,19 @@ class Task
   end
 
   define_singleton_method(:all) do
-    returned_tasks = DB.exec("SELECT * FROM task;")
+    returned_tasks = DB.exec("SELECT * FROM task ORDER BY due_date ASC;")
     tasks = []
     returned_tasks.each() do |task|
       description = task.fetch("description")
       list_id = task.fetch("list_id").to_i()
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      due_date = task.fetch("due_date")
+      tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
     end
     tasks
   end
 
   define_method(:save) do
-    DB.exec("INSERT INTO task (description, list_id) VALUES ('#{@description}', #{@list_id})")
+    DB.exec("INSERT INTO task (description, list_id, due_date) VALUES ('#{@description}', #{@list_id}, '#{@due_date}')")
   end
 
   define_singleton_method(:clear) do
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,8 +3,8 @@ require('spec_helper')
 describe (Task) do
   describe ('#==') do
     it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "Pass Code Review", :list_id => 1})
-      task2 = Task.new({:description => "Pass Code Review", :list_id => 1})
+      task1 = Task.new({:description => "Pass Code Review", :list_id => 1, :due_date => "2015-01-05"})
+      task2 = Task.new({:description => "Pass Code Review", :list_id => 1, :due_date => "2015-01-05"})
       expect(task1).to(eq(task2))
     end
   end
@@ -17,24 +17,33 @@ describe (Task) do
 
   describe('#save') do
     it("adds a task to saved task array") do
-      test_task = Task.new({:description => "Pass Code Review", :list_id => 1})
+      test_task = Task.new({:description => "Pass Code Review", :list_id => 1, :due_date => "2016-01-05"})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
-    end
+      test_task2 = Task.new({:description => "Yep", :list_id => 2, :due_date => "2015-01-05"})
+      test_task2.save()
+      expect(Task.all()).to(eq([test_task2, test_task]))
+      end
   end
 
   describe("#description") do
     it("lets you read the description out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2015-01-05"})
       expect(test_task.description()).to(eq("learn SQL"))
     end
   end
 
   describe("#list_id") do
     it("lets you read the list ID out") do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2015-01-05"})
       expect(test_task.list_id()).to(eq(1))
     end
   end
 
+  describe("#due_date") do
+    it("lets you read the due date") do
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => "2015-01-05"})
+      expect(test_task.due_date()).to(eq("2015-01-05"))
+    end
+  end
 end
